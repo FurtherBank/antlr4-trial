05516fb9b2253e665fd19320ccf472dc8a2a109e
furtherbank <1293874885@qq.com>
2023-03-16 10:34:10 +0800

    feat: lexer & phaser

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { ChangeEvent, useCallback, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ExprLexer } from './dsl/parsers/ExprLexer';
+import { CharStreams, CommonTokenStream, Token } from 'antlr4ts';
+import { ExprParser } from './dsl/parsers/ExprParser';
 
 function App() {
+  const [text, setText] = useState('10*20+30');
+
+  const changeHandler = useCallback(
+    (event: ChangeEvent<HTMLTextAreaElement>) => {
+      setText(event.target.value);
+    },
+    [setText]
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea name="" id="" cols={30} rows={10} onChange={changeHandler}>
+        {text}
+      </textarea>
+      <button onClick={() => {
+        const input = CharStreams.fromString(text);
+        const lexer = new ExprLexer(input)
+        const tokens = new CommonTokenStream(lexer)
+        const parser = new ExprParser(tokens);
+        const tree = parser.expr()
+        // 拿 token
+        console.log(tokens.getTokens());
+        // 拿 tree
+        console.log(tree);
+        console.log(tree.toStringTree());
+      }}>click me</button>
     </div>
   );
 }
